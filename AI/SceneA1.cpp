159634a35143c06870f447ca57bab740727f4fce
TurtleCrusher47 <111266586+TurtleCrusher47@users.noreply.github.com>
2023-12-11 01:29:22 +0800

    Fix file errors

---
+++ AI/SceneA1.cpp
@@ -0,0 +1,11 @@
+#include "SceneA1.h"
+#include "GL\glew.h"
+#include "Application.h"
+#include <sstream>
+#include "StatesFish.h"
+#include "StatesShark.h"
+#include "StatesFishFood.h"
+#include "SceneData.h"
+#include "PostOffice.h"
+#include "ConcreteMessages.h"
+
---
+++ AI/SceneA1.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <map>
+#include <vector>
+#include <queue>
+#include "GameObject.h"
+#include "SceneBase.h"
+#include "ObjectBase.h"
+#include "StateMachine.h"
+
+class SceneA1 : public SceneBase, public ObjectBase
+{
+
+};
+
